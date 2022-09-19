838ac0d68810260edb27bf0f66906b4e226ebe6f
SergeyKonst3 <79882629+SergeyKonst3@users.noreply.github.com>
2022-09-19 16:53:35 -0700

    Update build.sh
    Changed to proper filename output of the ROM file.

--- build.sh
+++ build.sh
@@ -22,9 +22,9 @@ docker run --rm -it -v $PWD:/home/coreboot/coreboot \
 	-w /home/coreboot/coreboot coreboot/coreboot-sdk:0ad5fbd48d \
 	/bin/bash -c "make olddefconfig && make"
 
-cp build/coreboot.rom protectli_vp4630_$version.rom
+cp build/coreboot.rom protectli_vp4630_v$version.rom
 if [ $? -eq 0 ]; then
-	echo "Result binary placed in $PWD/protectli_vp4630_$version.rom" 
+	echo "Result binary placed in $PWD/protectli_vp4630_v$version.rom" 
 	sha256sum protectli_vp4630_v$version.rom > protectli_vp4630_v$version.rom.sha256
 else
 	echo "Build failed!"
